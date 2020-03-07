2fef2f65e6aee4acab493624d4f4f1d8323020f8
Robert Stojan <robert.stojan@protonmail.com>
2020-03-07 11:37:32 -0600

    fixing penny function

--- penny_counter.py
+++ penny_counter.py
@@ -1,6 +1,6 @@
-def(dollars, pennies=0):
+def number_of_pennies(dollars, pennies=0):
     pennies_in_dollars = dollars * 100
-    return dollars + pennies
+    return pennies_in_dollars + pennies
 
 print(number_of_pennies(5, 6)) # Should print 506
 print(number_of_pennies(4))    # Should print 400
